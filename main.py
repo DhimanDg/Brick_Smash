88dc7631ef2999edfa50ac6c731eb80eab63af40
Dhiman Dasgupta <dhimandasgupta1@gmail.com>
2026-03-22 07:04:52 -0500

    Bgimg and paddle created sucessfully

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import (NumberProperty , ReferenceListProperty, ObjectProperty, ColorProperty)
+from kivy.properties import (NumericProperty , ReferenceListProperty, ObjectProperty, ColorProperty)
 from kivy.vector import Vector
 from random import randint
 from kivy.clock import Clock
@@ -11,12 +11,12 @@ from kivy.core.audio import SoundLoader
 
 
 class Ball(Widget):
-    color = ColorProperty(1,1,1,1) #set to white for now
+    color = ColorProperty((1,1,1,1)) #set to white for now
 
 
 
-    velocity_x = NumberProperty(0)
-    velocity_y = NumberProperty(0)
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
 
 
     velocity = ReferenceListProperty(velocity_x, velocity_y) #This makes it easier to control the ball as the x and y axis vel can be called together
@@ -32,8 +32,8 @@ class Ball(Widget):
 
 
 class Paddle(Widget): 
-    score = NumberProperty(0)
-    color = ColorProperty(1, 1, 1, 1) #For now the color is set to white
+    score = NumericProperty(0)
+    color = ColorProperty((1, 1, 1, 1)) #For now the color is set to white
 
 
     def bounce_ball(self, ball):  # self refers to current object instance and ball is another parameter passed
@@ -55,7 +55,7 @@ class BrickGame(Widget):
                                 #once linked you can access it as self.ball inside the class
 
     player = ObjectProperty(None)
-    color = ColorProperty(1,1,1,1) # white
+    color = ColorProperty((1,1,1,1)) # white
 
 
 
@@ -71,6 +71,52 @@ class BrickGame(Widget):
 
 
         self.ball.move()
+
+
+    #Bounce off paddle
+
+        self.player.bounce_ball(self.ball)
+
+
+        #bounce off top and bottom of the screen logic. Will be updated later!!
+
+        if (self.ball.y < 0) or (self.ball.top > self.height): #height is the top of the window and top is the top of the ball widget
+            self.ball.velocity_y *= -1
+
+        #bounce of top and bottom, This part will be updated later!!
+
+        if (self.ball.x < self.x) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
+
+
+
+class BrickApp(App):
+
+
+
+    def build(self):
+        game = BrickGame()
+        game.serve_ball()
+
+
+
+
+        #This is where the game updated regularly using kivy built in clock. (Need to know more!)
+
+        Clock.schedule_interval(game.update, 1.0/60.0)
+
+
+        return game
+    
+
+if __name__ == "__main__":
+
+    BrickApp().run()
+
+
+        
+
 
 
 
